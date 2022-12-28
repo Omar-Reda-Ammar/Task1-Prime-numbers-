c574a5f5cf36812c98c6dd21c4cb3dfc327d327b
Omar-Reda-Ammar <113676982+Omar-Reda-Ammar@users.noreply.github.com>
2022-12-28 16:25:22 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1___Prime_Numbers__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //code for printing prime numbers
+            int x;
+            Console.Write("Enter start number : ");
+            int A = int.Parse(Console.ReadLine());
+            Console.Write("Enter end number : ");
+            for(int B = int.Parse(Console.ReadLine());
+                A<=B;
+                A++)
+            {
+                for (x=2; x<=A; x++)
+                if(A%x==0)
+                    {
+                        break;
+                    }
+                if (A==x)
+                {
+                    Console.WriteLine(A);
+                }
+            }
+        }
+    } 
+}
